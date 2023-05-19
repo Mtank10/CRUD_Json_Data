7b29147f8e9102704f00d8f90d7696cc212b962f
shivam raj <shivamraj@pop-os.localdomain>
2023-05-19 11:52:34 +0530

    first commit

---
+++ src/Data.js
@@ -0,0 +1,17 @@
+export const userList =[
+    {
+        name:"shivam raj",
+        email:"shivamraj@gmail.com",
+        id:1
+    },
+    {
+        name:"vishnu raj",
+        email:"vishnuraj@gmail.com",
+        id:2
+    },
+    {
+        name:"Nishant",
+        email:"Nishant12@gmail.com",
+        id:3
+    },
+]
--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useReducer } from 'react';
+import { configureStore } from '@reduxjs/toolkit';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import UserReducer from './UserReducer'
+import 'bootstrap/dist/css/bootstrap.css';
+const store = configureStore({
+    reducer:{
+      users:UserReducer
+    }
+})
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <Provider store={store}>
+        <App/>
+        </Provider>
+    </React.StrictMode>
+)
